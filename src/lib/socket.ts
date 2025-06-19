d4e09d2e8ae84743aac07b6f1c9c4658c035a41d
Aniket <aniketraj28042003@gmail.com>
2025-06-19 21:15:56 +0530

    add

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,6 +2,6 @@ import { backendUrl } from "@/config/envConfig";
 
 export function connectSocket(id: string) {
   const url = backendUrl.split("://")[1];
-  const socket = new WebSocket(`ws://${url}?userId=${id}`);
+  const socket = new WebSocket(`wss://${url}?userId=${id}`);
   return socket;
 }
